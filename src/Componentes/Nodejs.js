266902a3917c4126a524ede81d361d9df97e5cc2
GastonMGrecco <gastonmgrecco@gmail.com>
2022-04-04 16:16:42 -0500

    Agregué backen-pelis

--- src/Componentes/Nodejs.js
+++ src/Componentes/Nodejs.js
@@ -1,9 +1,17 @@
 import React from "react";
 import backendEcommerce from "./imagenes/backend-ecommerce.png";
-
+import backendPelis from "./imagenes/backend-pelis.png";
 const Nodejs = () => {
   return (
     <div className="portafolio">
+      <a
+        className="LinkP"
+        href="https://documenter.getpostman.com/view/19617428/UVyuQuM8"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <img alt="imagen" className="Lenguaje" src={backendPelis} />
+      </a>
       <a
         className="LinkP"
         href="https://documenter.getpostman.com/view/19617428/UVyrVcZR"
